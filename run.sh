a615714d2572985faddc523dfd57dfa8c23a2a62
carlos <carlos@beeva.(none)>
2013-08-21 13:23:21 +0200

    uses in memory database

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 roo script CreateRooProject.roo
 
-mysql test < conf/users.sql
-cp conf/persistence.xml ./src/main/resources/META-INF/persistence.xml 
-cp conf/applicationContext-security.xml ./src/main/resources/META-INF/spring/applicationContext-security.xml
+#mysql test < conf/users.sql
+#cp conf/persistence.xml ./src/main/resources/META-INF/persistence.xml 
+#cp conf/applicationContext-security.xml ./src/main/resources/META-INF/spring/applicationContext-security.xml
 
 mvn jetty:run
